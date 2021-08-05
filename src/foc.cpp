1568dd996676c4245950b3c09baf73d4591a6fde
David J. Fiddes <D.J@fiddes.net>
2021-08-05 22:09:22 +0100

    Fix integer overflow in FOC constant and sqrt calculation
    The modMax constant of 2/sqrt(3) in the FOC calculation
    has a signed integer overflow. This results in undefined
    behaviour and ends up with a different on C2000/x86_64
    compared to ARM. The fix is to evaluate in an external
    calulator and store as a floating point constant similar to the
    other constants for FOC.
    
    The fpsqrt method was forcing all input values to be
    INT32_MAX rather than clamping values above this to
    INT32_MAX. Fixed by using MIN() rather than MAX().
    
    Tests:
     - Run against new FOC test suite

--- src/foc.cpp
+++ src/foc.cpp
@@ -39,7 +39,7 @@ static const s32fp lqminusld = FP_FROMFLT(0.0058);
 static const s32fp sqrt3 = SQRT3;
 static const s32fp sqrt3inv1 = FP_FROMFLT(0.57735026919); //1/sqrt(3)
 static const s32fp zeroOffset = FP_FROMINT(1);
-static const int32_t modMax = FP_DIV(FP_FROMINT(2U), sqrt3);
+static const int32_t modMax = FP_FROMFLT(1.154700538379); // 2.0/sqrt(3.0);
 static const int32_t modMaxPow2 = modMax * modMax;
 static const int32_t minPulse = 1000;
 static const int32_t maxPulse = FP_FROMINT(2) - 1000;
@@ -160,7 +160,7 @@ uint32_t FOC::sqrt(uint32_t rad)
 
 u32fp FOC::fpsqrt(u32fp rad)
 {
-   rad = MAX(INT32_MAX,rad);
+   rad = MIN(INT32_MAX,rad);
    s32fp sqrt = RADSTART((s32fp)rad);
    s32fp sqrtl;
 
