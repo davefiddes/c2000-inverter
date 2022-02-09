231a6bef4abdcecd45ff20fcc449e040e92e5caa
David J. Fiddes <D.J@fiddes.net>
2022-02-09 11:49:34 +0000

    Integate gate driver support into main C2000 inverter
    Update the c2000 inverter application to use the gate driver.
    This involves turning on the gate drive PSU before initialising
    the gate drivers for each phase. The fault state of the gate
    drivers is reported to stdout every second.
    
    Add a simple heartbeat LED toggle to the main loop to
    compensate for the gate drive fault LED no longer being on (!)
    as a way of determinig if the inverter is powered up.
    
    Tests:
     - Run on the Tesla M3 inverter and verify it operates with the
       gate drive fault LED extinguished and not gate drive fault is
       reported.

--- platform/c2000/inverter_drivers/gatedriver.cpp
+++ platform/c2000/inverter_drivers/gatedriver.cpp
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "gatedriver.h"
+#include "c2000/gatedriver.h"
 #include "crc8.h"
 #include "device.h"
 #include "driverlib.h"
